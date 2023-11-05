e354cb7ed6e6e6724e64282b5b3a0b1bc2637f51
valiyaajay321 <147832404+valiyaajay321@users.noreply.github.com>
2023-11-05 20:55:40 +0530

    Update recharge.c

--- recharge.c
+++ recharge.c
@@ -1,9 +1,9 @@
 void Recharge() {
-    int choicePlan, len3;
-    char mobileNumber[10];
+    int d;
+
     printf("Enter Mobile Number: ");
-    scanf("%s", mobileNumber);
-    int len3=strlen(mobileNumber);
+    scanf(" %[^\n]",MobileNumber);
+    int len3=strlen(MobileNumber);
     if(len3==10){
     printf("\nSelect a Plan, e.g. 239 or 28 days\n");
     printf("1. ₹19    Validity- 1 Days     Data- 1.5GB\n");
@@ -14,16 +14,25 @@ void Recharge() {
     printf("6. ₹3,359  Validity- 365 Days   Data- 2.5GB/Day\n");
     printf("7. ₹1,799  Validity- 365 Days   Data- 2GB/Day\n");
     printf("\nEnter your choice: ");
-    scanf("%d", &choicePlan);
-    switch (choicePlan) {
+    scanf("%d", &d);
+
+    switch (d) {
+        int e;
         case 1: case 2: case 3: case 4: case 5:
         case 6: case 7:
-            printf("PROCEED TO PAY\n");
+             printf("Select an option to pay\n");
+            printf("  1.Paytm UPI\n");
+            printf("  2.Wallet / Postpaid\n");
+            printf("  3.Credit / Debit / ATM card\n");
+            printf("  4.Net Banking\n"); 
+            printf("Select an option to Pay : ");
+            scanf("%d",&c);
+           Recharge3();
             break;
             default :
             printf("Wrong Input\n");
             printf("Please Select a Valid Plan Number\n");
-            paymentMethodSelection();
+            Recharge();
     }
     }
     else{
@@ -31,77 +40,43 @@ void Recharge() {
         Recharge();
     }
 }
-void paymentMethodSelection(){
-    int reEnterChoice, paymentMethod;
-    printf("Please Re-Enter your choice Number : ");
-    scanf("%d",&reEnterChoice);
-    switch (reEnterChoice) {
-        case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-            printf("PROCEED TO PAY\n");
-            printf("Select an option to pay\n ");
-            printf("1.Paytm UPI\n");
-            printf("2.Wallet / Postpaid\n");
-            printf("3. Credit / Debit / ATM card\n");
-            printf("4.Net Banking\n"); 
-            printf("Select an option to Pay : ");
-            scanf("%d",&paymentMethod);
-            switch(paymentMethod)
-            {
-                case 1: case 2: case 3: case 4:
-                printf("Pay Securely");
-                break;
-                default :
-                printf("Wrong Input\n");
-                invalidChoiceNumber();
-            }
-            
-            break;
-            default :
-            printf("Wrong Input\n");
-            paymentMethodSelection();
-    }
-            
-}
-void invalidChoiceNumber(){
-    int reEnterChoiceNumberAgain;
+
+void Recharge2(){
      printf("Please Re-Enter your choice Number : ");
-     scanf("%d",&reEnterChoiceNumberAgain);
-     switch(reEnterChoiceNumberAgain){
-          case 1: case 2: case 3: case 4:
-                printf("Pay Securely");
-                break;
-                default :
-                printf("Wrong Input\n");
-                invalidChoiceNumber();
-     }
+     scanf("%d",&c);
+     Recharge3();
 }
-void PayBills() {
-    int billChoice, electricityBoard;
-    long long int consumerNumber;
-    printf("\n");
-    printf("1. Electricity Bill Payment\n");
-    printf("2. Water and Sewage Bill Payment\n");
-    printf("3. Gas Cylinder Bill Payment\n");
-    printf("------------------------------\n");
-    printf("enter your choice number: ");
-    scanf("%d", &billChoice);
-    if (billChoice == 1) {
-        printf("1. Paschim Gujarat Vij Company Limited\n");
-        printf("2. Madhya Gujarat Vij Company Limited\n");
-        printf("3. Dasshin Gujarat Vij Company Limited\n");
-        printf("4. Uttar Gujarat Vij Company Limited\n");
-        printf("5. Vaghani Energy Limited\n");
-        printf("-------------------------------\n");
-        printf("Select your electricity board name: ");
-        scanf("%d", &electricityBoard);
-    }
-    switch (electricityBoard) {
-        case 1: case 2: case 3: case 4: case 5:
-            printf("Please enter your 5 or 11 digit Consumer Number: ");
-            scanf("%lld", &consumerNumber);
-            printf("PROCEED TO PAY");
-            break;
-        default:
-            printf("Wrong Input");
-    }
+void Recharge3(){
+     switch(c){
+         int e;
+                case 1: 
+                 printf("ENTER 4-DIGIT UPI PIN : ");
+                scanf("%d",&e);
+                printf("\npayment Succefull");
+                break;
+                case 3:
+                long long int x1;
+                int x2,x3;
+                printf("Enter card Number : ");
+                scanf("%lld",&x1);
+                printf("\nEnter expiry / Validity : ");
+                scanf("%d",&x2);
+                printf("\n Enter CVV : ");
+                scanf("%d",&x3);
+                printf("\nPyment Succesfull");
+                break;
+                case 4:
+                 printf("Select Bank\n");
+    printf("1. State Bank of India (SBI)\n");
+    printf("2. ICICI Bank\n");
+    printf("3. Axis Bank\n");
+    printf("4. HDFC Bank\n");
+    printf("5. Union Bank of India\n");
+    printf("6. Central Bank of India\n");
+    break;
+          default :
+          printf("Wrong input");
+              Recharge2();  
+                
+            }
 }
