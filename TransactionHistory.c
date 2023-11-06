948e504e9348816a857b4cc99d3e8f16e65b82ad
valiyaajay321 <147832404+valiyaajay321@users.noreply.github.com>
2023-11-06 22:08:39 +0530

    Create TransactionHistory.c

---
+++ TransactionHistory.c
@@ -0,0 +1,18 @@
+
+
+
+
+
+  void TransactionHistory(){
+      if(TotalAmount>10000000){
+          printf("Rs. %lld  Received from xyz  (30 Sep 2023) ",Depos);
+      }
+      else{
+          printf("Rs. %lld Paid to xyz (5 Oct 2023)",Trans);
+      }
+  }
+ 
+  
+  
+  
+  
