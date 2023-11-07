3790c74f12d28b538685db4a86ccc62704f583da
valiyaajay321 <147832404+valiyaajay321@users.noreply.github.com>
2023-11-07 14:14:22 +0530

    Create deposit.c

---
+++ deposit.c
@@ -0,0 +1,33 @@
+
+void Deposit() {
+    printf("Enter your 10 Digit Account Number: ");
+    scanf(" %[^\n]",AccountNumber);
+    int len1 = strlen(AccountNumber);
+    if (len1 == 10) {
+        printf("Confirm your 10 Digit Account Number: ");
+         scanf(" %[^\n]", ConfirmAccountNumber);
+        int len2 = strlen(ConfirmAccountNumber);
+            if (len2 == 10) {
+                if (strcmp(AccountNumber,ConfirmAccountNumber) == 0) {
+                    printf("Enter Account Holder Name : ");
+        scanf(" %[^\n]",HolderName);
+                    printf("Enter the Amount you want to Deposit: ");
+                    scanf("%lld", &Depos);
+                    TotalAmount = TotalAmount + Depos;
+                    printf("A/c 3XXXXX6925 credited by Rs. %lld \nTotal Bal: Rs. %lld\nNever share OTP/Password for EMI postponement or any reason.\n", Depos, TotalAmount);
+                    printf("-------------------------------------------------------------\n");
+                    main();
+                } else {
+                    printf("Account Number and Confirm Account Number are not the same\n");
+                    Deposit();
+                }
+            } else {
+                printf("Please Enter a Valid 10 digit Confirm Account Number\n");
+                Deposit();
+            }
+       
+    } else {
+        printf("Please Enter a Valid 10 digit Account Number\n");
+        Deposit();
+    }
+}
