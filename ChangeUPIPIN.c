be9d0f689841f9ade7563a66479a64b5e814c066
Yesha Pandya <89733331+yeshapan@users.noreply.github.com>
2023-11-12 16:09:43 +0530

    Update Change_UPI_PIN.c

--- ChangeUPIPIN.c
+++ ChangeUPIPIN.c
@@ -6,16 +6,17 @@ void Change_UPI_PIN(){
     printf("\nENTER 4-DIGIT OLD UPI PIN : ");
     scanf("%d",&old_UPI_PIN);
     if(old_UPI_PIN==1234){
-       Change_UPI_PIN2();
+       reset_UPI_PIN();
+       //Change_UPI_PIN2 -> reset_UPI_PIN
     }
     else{
         printf("INVALID OLD UPI PIN\n");
         printf("plese Re-Enter your UPI PIN\n");
-        Change_UPI_PIN2();
+        reset_UPI_PIN();
     }
    
 }
- void Change_UPI_PIN2(){
+ void reset_UPI_PIN(){
          printf("ENTER 4-DIGIT NEW UPI PIN : ");
         scanf("%d",&new_UPI_PIN);
          printf("CONFIRM 4-DIGIT NEW UPI PIN : ");
@@ -25,6 +26,6 @@ void Change_UPI_PIN(){
         }
         if(new_UPI_PIN!=Confirm_UPI_PIN){
         printf("PIN didn't match\n");
-        Change_UPI_PIN2();
+        reset_UPI_PIN();
         }
     }
